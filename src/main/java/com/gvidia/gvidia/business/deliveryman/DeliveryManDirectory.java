133d00181d0c0a748f04ba44fa0ba7f2282acdcc
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-12-01 19:10:00 +0500

    these files creates an object deliveryman which has all credentials and access to the order details

---
+++ src/main/java/com/gvidia/gvidia/business/deliveryman/DeliveryManDirectory.java
@@ -0,0 +1,26 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.gvidia.gvidia.business.deliveryman;
+//
+//import com.gvidia.gvidia.business.base.GenericDirectory;
+//
+///**
+// *
+// * @author kaustubh
+// */
+//public class DeliveryManDirectory extends GenericDirectory<DeliveryMan>{
+//
+//
+//    public DeliveryMan add(String name,String phoneNumber, String userName, String password) {
+//
+//        DeliveryMan customer = DeliveryMan.createNewDeliveryMan(getList().size(), name,  phoneNumber, userName, password);
+//
+//        add(customer);
+//
+//        return customer;
+//
+//    }
+//}
