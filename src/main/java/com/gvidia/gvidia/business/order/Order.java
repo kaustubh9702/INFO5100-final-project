4314f451cba98470f5396f2eb9fdced6132032d6
mitesh699 <miteshsingh@Miteshs-MacBook-Air.local>
2022-12-07 22:34:00 +0500

    Creating an instance of placing an order with regards to the ordered item and quantity

---
+++ src/main/java/com/gvidia/gvidia/business/order/Order.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gvidia.gvidia.business.order;
+
+import com.gvidia.gvidia.business.base.Identifier;
+import com.gvidia.gvidia.business.enterprises.supplier.wr.SourceMaterialWorkRequest;
+import com.gvidia.gvidia.business.useraccount.UserAccount;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Mitesh
+ */
+public class Order extends Identifier{
+    
+    List<OrderItem> mOrderItems;
+    UserAccount mAccount;
+
+    private static int count = 0;
+    
+    public Order(List<OrderItem> mOrderItems, UserAccount account) {
+        super(count++);
+        this.mOrderItems = new LinkedList<>();
+        this.mOrderItems.addAll(mOrderItems);
+        mAccount = account;
+    }
+
+    public List<OrderItem> getmOrderItems() {
+        return mOrderItems;
+    }
+
+    public void setmOrderItems(List<OrderItem> mOrderItems) {
+        this.mOrderItems = mOrderItems;
+    }
+
+    public double total() {
+        double sum = 0;
+        
+        
+        for(OrderItem item : mOrderItems) {
+            sum += item.getPrice();
+        }
+        return sum;
+    }
+
+    @Override
+    public void delete() {
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Order that = (Order) o;
+        return super.equals(that);
+    }
+
+    public int items() {
+        int items = 0;
+        for(OrderItem i: getmOrderItems()) {
+            items += i.getmQuantity();
+        }
+        return items;
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
